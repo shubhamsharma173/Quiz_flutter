2e8e81bfdc2822b624db1e7ee439dfd407cb172c
YashVerma421 <vermayash421@gmail.com>
2022-11-06 11:46:10 +0530

    fix video

--- lib/screens/video_player/video_player.dart
+++ lib/screens/video_player/video_player.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quiz_app/screens/score/score_screen.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:async';
 
@@ -14,16 +16,33 @@ class _VideoPlayerState extends State<VideoPlayerWidget> {
   @override
   void initState() {
     super.initState();
+
     _videoPlayerController =
-        VideoPlayerController.network(
-          'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+        VideoPlayerController.asset(
+          'assets/video/product.mp4',
         );
     _initializeVideoPlayerFuture = _videoPlayerController.initialize();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight
+    ]);
+
     _videoPlayerController.play();
+    _videoPlayerController.addListener(() {
+      if(_videoPlayerController.value.position == _videoPlayerController.value.duration && _videoPlayerController.value.duration > Duration(seconds: 1)) {
+        print('video Ended-------------------');
+        dispose();
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>  ScoreScreen()));
+      }
+    });
   }
 
   @override
   void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
     _videoPlayerController.dispose();
     super.dispose();
   }
